91c36e94676f129c78c6d90b1382180bf1e9abf3
arron1118 <arron1118@icloud.com>
2022-09-14 09:52:32 +0800

    新增套房排查编辑功能

--- app/api/controller/Investigation.php
+++ app/api/controller/Investigation.php
@@ -178,7 +178,6 @@ class Investigation extends ApiController
         if ($request->isPost()) {
             $params = $request->only(['title', 'images', 'image_time', 'description', 'crack_area', 'crack_sum', 'crack_description', 'refuse_reason']);
             $params['user_id'] = $this->userInfo->id;
-            $params['type'] = (int) $params['type'];
 
             if (!$id) {
                 $this->returnApiData('未找到套房排查ID：id');
